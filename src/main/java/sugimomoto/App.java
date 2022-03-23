4f71cc5b43c6e3d17dc34e113ae95f29dca60e02
sugimomoto <sugimomoto@gmail.com>
2022-03-24 07:54:31 +0900

    Added crud query

--- src/main/java/sugimomoto/App.java
+++ src/main/java/sugimomoto/App.java
@@ -13,9 +13,14 @@ public class App
         System.out.println( "Hello World!" );
 
         Class.forName ("org.h2.Driver");
-        Connection conn = DriverManager.getConnection ("jdbc:h2:~/test", "sa","");
+        Connection conn = DriverManager.getConnection ("jdbc:h2:~/testdb", "sa","");
 
         Statement st = conn.createStatement();
+
+        st.execute("DROP TABLE IF EXISTS TEST;");
+        st.execute("CREATE TABLE TEST(ID INT PRIMARY KEY, NAME VARCHAR(255));");
+        st.execute("INSERT INTO TEST VALUES(1, 'Hello');");
+        st.execute("INSERT INTO TEST VALUES(2, 'World');");
 
         ResultSet rs = st.executeQuery("SELECT * FROM TEST");
         
